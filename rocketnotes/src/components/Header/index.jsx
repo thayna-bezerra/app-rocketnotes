c2def5a6b4870c79bc8c6b96fee476274f3d2307
thayna <arraisthayna2106@gmail.com>
2023-01-25 17:12:53 -0300

    navegação da aplicação

--- rocketnotes/src/components/Header/index.jsx
+++ rocketnotes/src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import { Container, Profile, Logout } from './style';
 export function Header() {
   return(
     <Container>
-      <Profile>
+      <Profile to="/profile">
         <img 
           src="https://github.com/thayna-bezerra.png"
           alt="Foto do usuário"
--- rocketnotes/src/pages/Home/style.js
+++ rocketnotes/src/pages/Home/style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
   width: 100%;
@@ -60,11 +61,11 @@ export const Content = styled.div`
   overflow-y: auto;
 `;
 
-export const NewNote= styled.button`
+export const NewNote= styled(Link)`
   grid-area: newnote;
 
-  background: ${({theme}) => theme.COLORS.ORANGE};
-  border: none;
+  background-color: ${({theme}) => theme.COLORS.ORANGE};
+  color: ${({theme}) => theme.COLORS.BACKGROUND_900};
 
   display: flex;
   align-items: center;
