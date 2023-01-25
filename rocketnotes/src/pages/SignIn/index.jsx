949367c59b0598ad962eb4f5f96df341baa2507c
thayna <arraisthayna2106@gmail.com>
2023-01-25 15:01:48 -0300

    pages sign in e sign up

---
+++ rocketnotes/src/pages/SignIn/index.jsx
@@ -0,0 +1,39 @@
+import {FiMail, FiLock} from 'react-icons/fi'
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
+import { Container, Form, Background } from "./style";
+
+export function SignIn(){
+  return(
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+        <h2>Faça seu Login</h2>
+
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Entrar"/>
+
+        <a href='#'>
+          Criar Conta
+        </a>
+      </Form>
+
+      <Background/>
+    </Container>
+  )
+}
