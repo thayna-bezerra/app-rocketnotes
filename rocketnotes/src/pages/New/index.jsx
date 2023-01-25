f6fecd08ed1b1e2f5d93e2bbfd72c14638796734
thayna <arraisthayna2106@gmail.com>
2023-01-25 16:32:05 -0300

    interface new

---
+++ rocketnotes/src/pages/New/index.jsx
@@ -0,0 +1,42 @@
+import { Textarea } from '../../components/Textarea';
+import { NoteItem } from '../../components/NoteItem';
+import { Section } from '../../components/Section';
+import { Header } from '../../components/Header';
+import { Button } from '../../components/Button';
+import { Input } from '../../components/Input';
+
+import { Container, Form } from "./style";
+
+export function New() {
+  return(
+    <Container>
+      <Header/>
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">Voltar</a>
+          </header>
+
+          <Input placeholder="Título"/>
+          <Textarea placeholder="Observações"/>
+
+          <Section title="Links Úteis">
+            <NoteItem value="https://github.com/thayna-bezerra"/>
+            <NoteItem isNew placeholder="Novo Link"/>
+          </Section>
+
+          <Section title="Marcadors">
+            <div className="tags">
+              <NoteItem value="react"/>
+              <NoteItem isNew placeholder="Nova Tag"/>            
+            </div>
+          </Section>
+
+        <Button title="Salvar"/>
+        </Form>
+      </main>
+    </Container>
+  );
+}
