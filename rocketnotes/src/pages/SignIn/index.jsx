527f8c7b8936f7652c391322bd067aef1c2da780
thayna <arraisthayna2106@gmail.com>
2023-01-27 19:20:31 -0300

    hook de autenticação e sign in

--- rocketnotes/src/pages/SignIn/index.jsx
+++ rocketnotes/src/pages/SignIn/index.jsx
@@ -1,8 +1,8 @@
-import { useContext } from 'react';
+import { useState } from 'react';
 import {FiMail, FiLock} from 'react-icons/fi'
 import { Link } from 'react-router-dom';
 
-import { MyContext } from '../../myContext';
+import { useAuth } from '../../hooks/auth';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
@@ -10,9 +10,14 @@ import { Button } from '../../components/Button';
 import { Container, Form, Background } from "./style";
 
 export function SignIn(){
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   
-  const data = useContext(MyContext);
-  console.log("MEU CONTEXTO => ", data);
+  const { signIn } = useAuth();
+
+  function handleSignIn(){
+    signIn({email, password});
+  }
 
   return(
     <Container>
@@ -26,15 +31,17 @@ export function SignIn(){
           placeholder="E-mail"
           type="text"
           icon={FiMail}
-        />
+          onChange = {e=> setEmail(e.target.value)}
+          />
 
         <Input
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange = {e=> setPassword(e.target.value)}
         />
 
-        <Button title="Entrar"/>
+        <Button title="Entrar" onClick={handleSignIn}/>
 
         <Link to="/register">
           Criar Conta
