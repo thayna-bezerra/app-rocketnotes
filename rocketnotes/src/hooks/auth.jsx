b0676df80d0cd2dc14fa984238469e75575d3465
thayna <arraisthayna2106@gmail.com>
2023-01-28 12:42:02 -0300

    utilizando local storage

--- rocketnotes/src/hooks/auth.jsx
+++ rocketnotes/src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 
 import { api } from '../services/api';
 
@@ -12,6 +12,9 @@ function AuthProvider({ children }) {
       const response = await api.post("/sessions", {email, password});
       const { user, token } = response.data;
 
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+      localStorage.setItem("@rocketnotes:token", token);
+
       api.defaults.headers.authorization = `Bearer ${token}`;
       setData({ user, token })
 
@@ -23,6 +26,21 @@ function AuthProvider({ children }) {
       }
     }
   }
+
+  useEffect(() => {
+    const user = localStorage.getItem("@rocketnotes:user");
+    const token = localStorage.getItem("@rocketnotes:token");
+
+    if(token && user){
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      setData({
+        token,
+        user: JSON.parse(user)
+      });
+    }
+    
+  }, []);
 
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
